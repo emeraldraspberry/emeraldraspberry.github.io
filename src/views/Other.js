c621827fc4021a9206555586736e54a8b375d60f
emeraldraspberry <48506213+emeraldraspberry@users.noreply.github.com>
2021-01-17 16:52:48 -0600

    Add text content to Other view

--- src/views/Other.js
+++ src/views/Other.js
@@ -6,7 +6,34 @@ const Other = {
       "div",
       { id: "other" },
       m("h2", "other stuff.."),
-      m("p", "I type programs and scripts with various tools.")
+      m(
+        "div",
+        m(
+          "p",
+          "I type programs and scripts with various tools. Sometimes for fun, sometimes for practice."
+        ),
+        m("p", "Current list of tools I use:"),
+        m(
+          "ul",
+          m("li", "Vue.js 3 + Vue Router"),
+          m("li", "Electron"),
+          m("li", "Node.js"),
+          m("li", "Wave UI"),
+          m("li", "SCSS"),
+          m("li", "Webpack"),
+          m("li", "Mithril"),
+          m("li", "Snowpack"),
+          m("li", "Userscript")
+        ),
+        m("p", "Not currently using but dealt with:"),
+        m(
+          "ul",
+          m("li", "Angular Typescript"),
+          m("li", "jQuery"),
+          m("li", "PyQt5"),
+          m("li", "C OpenGL")
+        )
+      )
     );
   },
 };
